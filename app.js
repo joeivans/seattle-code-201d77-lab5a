048f8807db0876691ff11533766e8849e629c875
Joe Ivans <joseph.ivans@gmail.com>
2021-07-16 23:14:43 -0700

    Add question 3 impl

--- app.js
+++ app.js
@@ -32,7 +32,11 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
   let product = a*b;
-  return [product, 'The product of ' + a + ' and ' + b + ' is ' + product + '.'];
+  let result = [product, 'The product of ' + a + ' and ' + b + ' is ' + product + '.'];
+
+  console.log(result);
+
+  return result;
 }
 
 // Here is the test for multiply(); uncomment it to run it
@@ -54,10 +58,18 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
 // your previous function sum returns an array. let answer = sum(10, 5)[0];
+// 1, 2, 3
+  let _sum = sum(a, sum(b, c)[0])[0];
+  let _product = multiply(a, multiply(b, c)[0])[0];
+  let result = [_sum, _product, a + ' and ' + b + ' and ' + c + ' sum to ' + _sum + '.', 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + _product + '.'];
+
+  console.log(result);
+
+  return result;
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
